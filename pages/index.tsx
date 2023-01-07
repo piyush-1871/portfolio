d55967a71f3989973244347ebe80d6c651b20547
piyush-1871 <piyush1871b@gmail.com>
2023-01-07 17:09:11 +0530

    setup tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +10,21 @@ export default function Home() {
       <Head>
         <title>Portfolio</title>
       </Head>
-      <h1>lets build awesome portfolio</h1>
+
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+
     </div>
   );
 }
