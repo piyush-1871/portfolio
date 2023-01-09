84aeb8e83269c937927d2c218ed2d3c4be844b6b
piyush-1871 <piyush1871b@gmail.com>
2023-01-10 01:35:57 +0530

    added tailwind scroll bar librabry

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll z-0 ">
+    <div className="bg-[rgb(36,36,36)]
+     text-white h-screen snap-y
+      snap-mandatory overflow-x-hidden 
+      overflow-y-scroll z-0 scrollbar-track-gray-400/20 
+      scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin">
       <Head>
         <title>Piyush's Portfolio</title>
       </Head>
