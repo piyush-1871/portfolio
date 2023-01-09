c919ef9d777e85d84c75bfff89878a96a94002ad
piyush-1871 <piyush1871b@gmail.com>
2023-01-09 23:09:38 +0530

    starting with Skill component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Experience from "../components/Experience";
+import Skills from "../components/Skills";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +35,9 @@ export default function Home() {
       </section>
 
       {/* Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
       {/* Projects */}
 
