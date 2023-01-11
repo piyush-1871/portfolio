674f8c2dc751717e94d4d735251155de0e74048c
piyush-1871 <piyush1871b@gmail.com>
2023-01-11 16:00:10 +0530

    changing and adding sanity schemas

---
+++ portfolio/schemas/index.js
@@ -0,0 +1,6 @@
+import pageInfo from './pageInfo'
+import experience from './experience'
+import project from './projects'
+import skill from './skill'
+import social from './social'
+export const schemaTypes = [pageInfo, experience, project, skill, social]
