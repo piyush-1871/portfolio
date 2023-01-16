50f4252f81a60822d528c4694cce69a3ca7d4e5b
piyush-1871 <piyush1871b@gmail.com>
2023-01-16 15:54:53 +0530

    added animation to skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,13 +1,14 @@
+import { Staatliches } from "@next/font/google";
 import { motion } from "framer-motion";
 import React from "react";
-import { Skill as SkillType} from "../typing";
+import { Skill as SkillType } from "../typing";
 import Skill from "./Skill";
 
 type Props = {
-  skills: SkillType[]
+  skills: SkillType[];
 };
 
-function Skills({skills}: Props) {
+function Skills({ skills }: Props) {
   return (
     <motion.div
       initial={{
@@ -42,10 +43,16 @@ function Skills({skills}: Props) {
         Hover over a skill for current proficiency
       </h3>
 
-      <div className="grid grid-cols-4 
-      gap-5 mt-16">
-        {skills?.map((skill)=>(
+      <div
+        className="grid grid-cols-4 
+      gap-5 mt-16"
+      >
+        {skills?.slice(0, skills.length /2).map((skill) => (
           <Skill key={skill._id} skill={skill} />
+        ))}
+
+        {skills?.slice(skills.length /2, skills.length).map((skill) => (
+          <Skill key={skill._id} skill={skill} directionLeft />
         ))}
       </div>
     </motion.div>
